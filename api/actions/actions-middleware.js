2642c702cc668d0916af5b228da453ce954d899c
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-13 08:43:57 -0500

    MVP complete!

--- api/actions/actions-middleware.js
+++ api/actions/actions-middleware.js
@@ -14,12 +14,45 @@ async function validActionId(req, res, next){
                 message: `No action with id of ${req.params.id}`
             })
         }
-        
+
     } catch (error) {
         next(error)
     }
 }
 
+async function validProject(req,res,next){
+    const { project_id } = req.body
+    const project = await Project.get(project_id)
+    try {
+        if(project){
+            next();
+        }else{
+            res.status(404).json({
+                message: `no project with id of ${project_id}`
+            })
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+function validActionInfo (req, res, next){
+    const { description, notes } = req.body;
+    if(  description &&
+         description.trim().length &&
+         description.length <= 128 &&
+         notes &&
+         notes.trim().length){
+        next()
+    }else{
+        res.status(400).json({
+            message: 'Action needs a description and notes.'
+        })
+    }
+}
+
 module.exports = {
     validActionId,
+    validProject,
+    validActionInfo,
 }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const {
  validActionId,
+ validProject,
+ validActionInfo,
 } = require('./actions-middleware');
  
 const Action = require('./actions-model')
@@ -22,6 +24,31 @@ router.get('/:id', validActionId, async (req, res, next) => {
     res.json(req.action);
 })
 
+router.post('/', validProject, validActionInfo, async (req, res, next) => {
+    const newAction = await Action.insert(req.body)
+    try {
+        res.status(201).json(newAction)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.put('/:id', validActionId, validActionInfo, async (req, res, next) => {
+    const update = await Action.update(req.params.id, req.body)
+    try{
+        res.json(update)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', validActionId, (req, res, next) => {
+    Action.remove(req.params.id)
+        .then(() => {
+            res.json()
+        })
+        .catch(next)
+})
 
 router.use((err, req, res, next) => { //eslint-disable-line
     res.status(err.status || 500).json({
